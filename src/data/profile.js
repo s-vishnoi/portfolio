c2ac7952bcab0cb517a447c89c3beb637fc1a8fe
Samvardhan Vishnoi <samvardhanvishnoi@gmail.com>
2025-04-14 18:16:07 -0500

    description

--- src/data/profile.js
+++ src/data/profile.js
@@ -1,8 +1,7 @@
 export const profile = {
   name: "Samvardhan Vishnoi",
   title: "Data Scientist",
-  description: `I harness data to investigate human behaviors and reimagine a more equitable, just, and transparent society. 
-<br> 
+  description: `I harness data to investigate human society and reimagine a future driven by transparency, equity, and justice.
 I'm always looking for meaningful problems, good collaborators, and the next system worth exploring.`,
   email: "svishnoi@u.northwestern.edu"
 };
