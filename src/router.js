64dac1e4b6ceba8b49a82876b0027cb42dae61d3
Samvardhan Vishnoi <samvardhanvishnoi@gmail.com>
2025-04-15 12:35:18 -0500

    route teaching

--- src/router.js
+++ src/router.js
@@ -4,13 +4,15 @@ import UrbanScaling from './pages/UrbanScaling.vue'
 import Demographics from './pages/Demographics.vue'
 import BlogArchive from './pages/BlogArchive.vue'
 import Resume from './pages/Resume.vue'
+import Teaching from './pages/Teaching.vue'
 
 const routes = [
   { path: '/', component: Home },
   { path: '/urban-scaling', component: UrbanScaling },
   { path: '/demographics', component: Demographics },
   { path: '/blog', component: BlogArchive },
-  { path: '/resume', component: Resume } 
+  { path: '/resume', component: Resume },
+  { path: '/teaching', component: Teaching }
 ]
 
 
