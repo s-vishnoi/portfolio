ac2425b67ed63fd56d8f307e5c27c5db9f500947
Samvardhan Vishnoi <samvardhanvishnoi@gmail.com>
2025-04-17 14:19:48 -0500

    updates

--- src/pages/Resume.vue
+++ src/pages/Resume.vue
@@ -1,6 +1,14 @@
 <template>
     <section class="bg-base-100 p-6 rounded-box shadow max-w-4xl mx-auto my-10">
-      <h2 class="text-2xl font-bold mb-4">Resume</h2>
+      <h2 class="text-2xl font-bold mb-4">Resume
+        <a
+        href="/resume.pdf"
+        download
+        class="btn btn-accent text-right"
+      >
+        Download PDF
+      </a>
+      </h2>
       <div class="aspect-[8.5/11] w-full mb-6">
         <iframe
           src="/resume.pdf"
@@ -8,13 +16,7 @@
           type="application/pdf"
         ></iframe>
       </div>
-      <a
-        href="/resume.pdf"
-        download
-        class="btn btn-accent"
-      >
-        Download PDF
-      </a>
+      
     </section>
 </template>
   
