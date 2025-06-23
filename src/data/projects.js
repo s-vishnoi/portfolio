73142df59c15605783a64ddabef639c93a938531
Samvardhan Vishnoi <samvardhanvishnoi@gmail.com>
2025-06-23 11:29:21 -0500

    photo bro!

--- src/data/projects.js
+++ src/data/projects.js
@@ -2,7 +2,7 @@ export const projects = [
   {
     title: "Chicago Bike Dashboard",
     description: "A community data resource for Chicago's bike infrastructure",
-    image: "/images/projects/bike_accidents.png",
+    image: "/images/projects/biking.png",
     link: "/bikeability"
   },
   {
