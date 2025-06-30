eecd8a87da3a0c1aeb66457c6499fd1ed864bfb5
Samvardhan Vishnoi <samvardhanvishnoi@gmail.com>
2025-06-30 16:57:51 -0500

    texts

--- src/pages/UrbanScaling.vue
+++ src/pages/UrbanScaling.vue
@@ -5,14 +5,19 @@
     <h1 class="text-3xl font-bold mt-4">Urban Scaling Laws</h1>
     <p>
       Most cities grow, but they don't grow evenly. Larger cities often have disproportionate increases in crime,
-      income, and innovation. I study the dynamics of scaling laws — mathematical relationships that describe
-      how various urban indicators grow with population size. These relationships have enormous potential in not 
-      only filling the gaps in our understanding of the present but also preparing us for future resource needs. 
+      income, and innovation.
     </p>
     <p>
-      Arrest data collected from law agencies acorss the U.S. is heavily missing due to a voluntary reporting program. 
-      A major challenge remains identfying missing and underreported values.  
+      I study the dynamics of scaling laws — mathematical relationships that describe
+      how various urban indicators grow with city population. These relationships have enormous potential in not 
+      only filling the gaps in our understanding of present but also preparing us for future resource needs. 
     </p>
+    <h2 class="text-1.5xl font-bold mt-2">U.S. Arrest Data</h2>
+    <p>
+      Arrest data collected from law agencies acorss the U.S. is heavily missing due to a voluntary reporting program (Uniform Crime Reporting, FBI).
+      Current criminology research often ignores this missingness, leading to biased conclusions.
+      Identfying missing/underreported values remains a major statistical challenge, preventing reliable research and weakening policy initiatives.
+        </p>
       
     <img src="/images/projects/urban-scaling.jpg" alt="Missing Arrest Data" class="w-full rounded shadow" />
     <figcaption class="text-sm text-gray-500 mt-1">
@@ -23,13 +28,13 @@
       State-of-the-art Bayesian estimations allow modeling for time and region variations heirarchically, while maintainting transparency. 
     </p>
 
-    <h1 class="text-1.5xl font-bold mt-2 mb-1">Publications</h1>
+    <h2 class="text-1.5xl font-bold mt-2">Publications</h2>
     <p class="italic">
       Changes in poverty and ethnic diversity predict the murder rate of US metropolitan statistical areas over a 60-year period <br>
       <span class="not-italic">(Second author, 2025, <em>Nature Human Behavior</em> — under review)</span>
     </p>
 
-    <h1 class="text-1.5xl font-bold mt-2 mb-1">Code</h1>
+    <h2 class="text-1.5xl font-bold mt-2">Code</h2>
     <p>
       The open-source data and code repository will be made available after publication.
     </p>
