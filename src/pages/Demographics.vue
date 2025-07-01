68f0210507be1c9bb0c6e788184f41f806725b04
Samvardhan Vishnoi <samvardhanvishnoi@gmail.com>
2025-06-30 21:02:46 -0500

    texts

--- src/pages/Demographics.vue
+++ src/pages/Demographics.vue
@@ -8,24 +8,24 @@
 
     <h1 class="text-3xl font-bold mt-4">Research Demographic Equality</h1>
     <p>
-      While representation in academia has been studied extensively in individual aspects
-      i.e. joining, dropping out, location, and mobility etc., comprehensive  
+      While representation in academia has been studied extensively from isolated lenses
+      i.e. seniority, productivity, mobility etc., comprehensive  
       ground truth data informing global equality remains missing.  
     </p>
     <p>
-      We leverage 45M+ research records to extract the career trajectories of 30M+ researchers (anonymized) in ~200 countries.
+      We leverage 45M+ research records to extract the career trajectories of 30M+ researchers (anonymized) across ~200 countries.
       We define and measure country-level academic growth, migration networks, and demographic equality- providing a novel global outlook for policy.
       
     </p>
 
     <h1 class="text-1.5xl font-bold mt-2 mb-1">Publications</h1>
-    <p class="italic">
+    <p class="italic mt-1">
       Global Improvements in the Representation of Women in Science Are Stalling <br>
       <span class="not-italic">(First author, 2025, <em>Science</em> — under review)</span>
     </p>
 
     <h1 class="text-1.5xl font-bold mt-2 mb-1">Code</h1>
-    <p>
+    <p class="mt-1">
       The open-source data and code repository will be made available after publication.
     </p>
   </main>
