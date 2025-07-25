96580842f48e93c52bce1d77c7f29e7408848240
Samvardhan Vishnoi <samvardhanvishnoi@gmail.com>
2025-07-24 19:31:53 -0500

    achievements

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -79,14 +79,14 @@
         <div class="bg-base-200 p-4 rounded-lg shadow flex items-center gap-3">
           <span class="text-2xl">🏅</span>
           <div>
-            <h3 class="font-semibold text-lg">Murray Green Medal</h3>
+            <h3 class="font-semibold text-gray-600">Murray Green Medal</h3>
             <!--<p class="text-sm text-gray-500 dark:text-gray-400">Awarded for excellence in Physics</p>-->
           </div>
         </div>
         <div class="bg-base-200 p-4 rounded-lg shadow flex items-center gap-3">
           <span class="text-2xl">🎓</span>
           <div>
-            <h3 class="font-semibold text-lg">Presidential Scholarship</h3>
+            <h3 class="font-semibold text-gray-600">Presidential Scholarship</h3>
             <!--<p class="text-sm text-gray-500 dark:text-gray-400">Merit-based academic scholarship</p>-->
           </div>
         </div>
