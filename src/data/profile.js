390fd76472a0b493fdaf6ad004e02e001ceec9d9
Samvardhan Vishnoi <samvardhanvishnoi@gmail.com>
2025-10-27 11:44:24 -0500

    collab

--- src/data/profile.js
+++ src/data/profile.js
@@ -1,6 +1,6 @@
 export const profile = {
   name: "Vishnoi",
-  title: "Systems Physicist | Data Scientist",
+  title: "Systems Physicist | Urban Data Scientist",
   description: `I harness data to investigate human society and reimagine a future driven by transparency, equity, and justice.`,
   availability: {
     label: "Open to collaborations"
