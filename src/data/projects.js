9b63f6ed78ccb08bb454c6afed84bfdc0888247b
Samvardhan Vishnoi <samvardhanvishnoi@gmail.com>
2025-10-27 14:31:53 -0500

    streamline projs

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,20 +1,20 @@
 export const projects = [
   {
     title: "Chicago Bikeability Map",
-    description: "A community data resource for exploring Chicago's bike infrastructure",
+    description: "A community map on biking safety & infrastructure in Chicago",
     image: "/images/projects/biking.png",
     link: "https://chicago-bike-dashboard.onrender.com",
     external: true
   },
   {
     title: "Urban Scaling Laws",
-    description: "Modeling US city socioeconomics using scaling laws from physics",
+    description: "Modeling US city trends using scaling laws from physics",
     image: "/images/projects/urban-scaling.jpg",
     link: "/urban-scaling"
   },
   {
     title: "Global Researcher Demographics",
-    description: "Leveraging publication records to study global research patterns and equality",
+    description: "Leveraging publication records to measure global research equality",
     image: "/images/projects/demography-map.png",
     link: "/demographics"
   }
