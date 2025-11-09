753fefaf7b0c23db643567b63026f7cf2f628665
Samvardhan Vishnoi <samvardhanvishnoi@gmail.com>
2025-11-09 17:28:21 -0600

    subtit

--- src/data/profile.js
+++ src/data/profile.js
@@ -1,6 +1,6 @@
 export const profile = {
   name: "Vishnoi",
-  title: "Physicist | Data Scientist",
+  title: "Physics | Data Science | Social Good",
   description: `I harness data to investigate human society and reimagine a future driven by transparency, equity, and justice.`,
   availability: {
     label: "Open to collaboration"
